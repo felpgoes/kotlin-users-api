8a6eb12ce0edd421f51f4c2b519ce47c28e0b552
felpgoes <felipe.goess@hotmail.com>
2024-02-08 18:07:19 -0300

    ci: remove useless dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,6 @@ dependencies {
 	runtimeOnly("com.oracle.database.jdbc:ojdbc11")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 
 	testImplementation("io.mockk:mockk:1.10.4")
 	testImplementation("com.ninja-squad:springmockk:3.0.1")
