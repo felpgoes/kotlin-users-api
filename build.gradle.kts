87eb4ff4629b8da0b1cf15b16861ad17229a0e81
felpgoes <felipe.goess@hotmail.com>
2024-02-08 11:16:17 -0300

    ci: creating Dockerfile and docker-compose.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ group = "com.kotlinspring"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_19
 }
 
 repositories {
@@ -44,7 +44,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
+		jvmTarget = "19"
 	}
 }
 
