2dd923a21888fefa08e97fb9f23738f166cc4d31
felpgoes <felipe.goess@hotmail.com>
2024-03-04 18:57:57 -0300

    feat: creating Job Repository

---
+++ src/main/kotlin/com/kotlinspring/crudkotlinpoc/repository/JobRepository.kt
@@ -0,0 +1,8 @@
+package com.kotlinspring.crudkotlinpoc.repository
+
+import com.kotlinspring.crudkotlinpoc.entitiy.Job
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface JobRepository: JpaRepository<Job, String>
